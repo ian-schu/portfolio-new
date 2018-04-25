79e1710e670b9ced4a5a345ff70d90cb273cefc3
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-24 22:12:58 -0500

    Content and style updates

--- scripts/ui.js
+++ scripts/ui.js
@@ -2,13 +2,9 @@ const getById = elementID => {
 	return document.getElementById(elementID);
 };
 
-// const navToggle = getById('navToggle');
 const navUL = getById('navUL');
 const gameboard = getById('gameboard');
 
-const bottomnav = getById('bottomnav');
-const prevButton = getById('prevButton');
-const nextButton = getById('nextButton');
 const playButton = getById('playButton');
 let playButtonText = playButton.children[1];
 
@@ -21,26 +17,3 @@ const viewportRatio = window.innerWidth / window.innerHeight;
 const toggleClass = (element, className) => {
 	element.classList.toggle(className);
 };
-
-nextButton.addEventListener('click', () => {
-	window.scrollBy({
-		top: 0.94 * window.innerHeight,
-		left: 0,
-		behavior: 'smooth'
-	});
-});
-
-prevButton.addEventListener('click', () => {
-	window.scrollBy({
-		top: -0.94 * window.innerHeight,
-		left: 0,
-		behavior: 'smooth'
-	});
-});
-
-document.addEventListener('scroll', () => {
-	bottomnav.style.opacity = 0;
-	setTimeout(() => {
-		bottomnav.style.opacity = 1;
-	}, 1500);
-});
