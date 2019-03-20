326b3d3d2a15950e12e8fdbd0de42f32c8fb61fa
Ian Schumann <ian.l.schumann@gmail.com>
2019-03-19 19:48:13 -0500

    Downsize content w/ 'needs update' msg

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@
 
 // Initialize Conway game board
 const resolveBoardDimensions = () => {
-	let width = deviceIsMobile ? 25 : 50;
-	let height = Math.round(width / viewportRatio);
-	return [width, height];
+  let width = deviceIsMobile ? 25 : 50;
+  let height = Math.round(width / viewportRatio);
+  return [width, height];
 };
 
 const [bWidth, bHeight] = resolveBoardDimensions();
@@ -16,61 +16,73 @@ const conwayControl = new Controller(conwayView, conwayModel);
 
 let notPlayedYet = true;
 
-gameboard.addEventListener('mousedown', conwayControl.cellClick, false);
-gameboard.addEventListener('mouseover', conwayControl.cellClick, false);
-gameboard.addEventListener('touchend', conwayControl.cellClick, false);
+gameboard.addEventListener("mousedown", conwayControl.cellClick, false);
+gameboard.addEventListener("mouseover", conwayControl.cellClick, false);
+gameboard.addEventListener("touchend", conwayControl.cellClick, false);
 
 // Start typey
-typeControl(likeSpan, blurbArray);
+setTimeout(() => typeControl(likeSpan, blurbArray), 3000);
 
 // Drop patterns
 const dropAllPatterns = () => {
-	if (!deviceIsMobile) {
-		const gliders = [[bHeight * 0.1, bWidth * 0.1], [bHeight * 0.25, bWidth * 0.4]];
-		const hives = [[bHeight * 0.65, bWidth * 0.1], [bHeight * 0.9, bWidth * 0.8]];
-		const blocks = [[bHeight * 0.27, bWidth * 0.7], [bHeight * 0.8, bWidth * 0.1]];
-		const spaceships = [[bHeight * 0.4, bWidth * 0.75], [bHeight * 0.68, bWidth * 0.55]];
+  if (!deviceIsMobile) {
+    const gliders = [
+      [bHeight * 0.1, bWidth * 0.1],
+      [bHeight * 0.25, bWidth * 0.4]
+    ];
+    const hives = [
+      [bHeight * 0.65, bWidth * 0.1],
+      [bHeight * 0.9, bWidth * 0.8]
+    ];
+    const blocks = [
+      [bHeight * 0.27, bWidth * 0.7],
+      [bHeight * 0.8, bWidth * 0.1]
+    ];
+    const spaceships = [
+      [bHeight * 0.4, bWidth * 0.75],
+      [bHeight * 0.68, bWidth * 0.55]
+    ];
 
-		dropPatterns(glider, gliders);
-		dropPatterns(hive, hives);
-		dropPatterns(block, blocks);
-		dropPatterns(lwss, spaceships);
-	} else {
-		const hives = [
-			[bHeight * 0.18, bWidth * 0.1],
-			[bHeight * 0.12, bWidth * 0.4],
-			[bHeight * 0.22, bWidth * 0.55],
-			[bHeight * 0.16, bWidth * 0.75]
-		];
-		const vSpaceships = [
-			[bHeight * 0.8, bWidth * 0.1],
-			[bHeight * 0.8, bWidth * 0.4],
-			[bHeight * 0.8, bWidth * 0.7]
-		];
-		dropPatterns(vlwss, vSpaceships);
-		dropPatterns(hive, hives);
-	}
+    dropPatterns(glider, gliders);
+    dropPatterns(hive, hives);
+    dropPatterns(block, blocks);
+    dropPatterns(lwss, spaceships);
+  } else {
+    const hives = [
+      [bHeight * 0.18, bWidth * 0.1],
+      [bHeight * 0.12, bWidth * 0.4],
+      [bHeight * 0.22, bWidth * 0.55],
+      [bHeight * 0.16, bWidth * 0.75]
+    ];
+    const vSpaceships = [
+      [bHeight * 0.8, bWidth * 0.1],
+      [bHeight * 0.8, bWidth * 0.4],
+      [bHeight * 0.8, bWidth * 0.7]
+    ];
+    dropPatterns(vlwss, vSpaceships);
+    dropPatterns(hive, hives);
+  }
 };
 
-playButton.addEventListener('click', () => {
-	if (notPlayedYet) {
-		notPlayedYet = false;
-		greeting.style.opacity = 0;
-		setTimeout(() => {
-			greeting.remove();
-		}, 1000);
-		setTimeout(() => {
-			dropAllPatterns();
-		}, 1600);
-		setTimeout(() => {
-			conwayControl.play();
-			playButtonText.textContent = 'Pause';
-		}, 2400);
-	} else if (conwayControl.isPlaying) {
-		conwayControl.stop();
-		playButtonText.textContent = 'Play';
-	} else {
-		conwayControl.play();
-		playButtonText.textContent = 'Pause';
-	}
+playButton.addEventListener("click", () => {
+  if (notPlayedYet) {
+    notPlayedYet = false;
+    greeting.style.opacity = 0;
+    setTimeout(() => {
+      greeting.remove();
+    }, 1000);
+    setTimeout(() => {
+      dropAllPatterns();
+    }, 1600);
+    setTimeout(() => {
+      conwayControl.play();
+      playButtonText.textContent = "Pause";
+    }, 2400);
+  } else if (conwayControl.isPlaying) {
+    conwayControl.stop();
+    playButtonText.textContent = "Play";
+  } else {
+    conwayControl.play();
+    playButtonText.textContent = "Pause";
+  }
 });
